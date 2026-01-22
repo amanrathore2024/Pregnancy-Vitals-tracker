30d021f3ee46f1d8b43d735d2c54e1684fceeeff
amanrathore2024 <rathoreaman1705@gmail.com>
2026-01-22 19:35:26 +0530

    Add util fun

---
+++ app/src/main/java/com/vitalstraker/presentation/utils/getCurrentFormattedDateTime.kt
@@ -0,0 +1,14 @@
+package com.vitalstraker.presentation.utils
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+fun getCurrentFormattedDateTime(): String {
+    val formatter = DateTimeFormatter.ofPattern(
+        "EEE, dd MMM yyyy hh:mm a",
+        Locale.ENGLISH
+    )
+
+    return LocalDateTime.now().format(formatter).lowercase()
+}
